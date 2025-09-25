f53e2bb88a0754865bb7ac64e3833a1d462a8068
PavelPugachenko <166284005+PavelPugachenko@users.noreply.github.com>
2025-09-25 12:29:07 +0500

    add bootstrap, base.html, fix servises

--- services/views.py
+++ services/views.py
@@ -1,3 +1,19 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Service
+
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, "services/list.html", {"services": services})
+
+
+def service_detail(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    return render(request, "services/detail.html", {"service": service})
+
+
+def home(request):
+    return render(request, "services/home.html")
 from django.shortcuts import render
 
 # Create your views here.
